d0b3cb1927b6c0ed996914ec1e0d3f245636be8e
Tim <spridl@mail.ru>
2022-03-08 00:29:32 +0300

    Create WelcomeViewController

---
+++ Login App/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Login App
+//
+//  Created by Тимур on 07.03.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func loginButtonPressed() {
+    }
+    
+    @IBAction func forgotUserButtonPressed() {
+    }
+    
+    @IBAction func forgotPasswordButtonPressed() {
+    }
+    
+
+}
+
---
+++ Login App/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  Login App
+//
+//  Created by Тимур on 08.03.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func logOutButtonPressed() {
+    }
+}
