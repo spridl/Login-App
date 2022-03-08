7995e607dfa5f33e4891dedf3c9f78b7ea5405bc
Tim <spridl@mail.ru>
2022-03-09 00:11:39 +0300

    Configuration buttons

--- Login App/LoginViewController.swift
+++ Login App/LoginViewController.swift
@@ -12,19 +12,59 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var userNameTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
+    private let userName = "User"
+    private let password = "password"
+     
     @IBAction func loginButtonPressed() {
+        if userNameTF.text != userName || passwordTF.text != password {
+            setAlertController(title: "Ошибка 🙀", message: "Неверные имя или пароль")
+            passwordTF.text = ""
+        }
     }
     
-    @IBAction func forgotUserButtonPressed() {
+    @IBAction func forgotButtonPressed(_ sender: UIButton) {
+        if sender.tag == 0 {
+            setAlertController(title: "Подсказка 🤫", message: "Имя пользователя: User")
+        } else {
+            setAlertController(title: "Подсказка 🤫", message: "Пароль: password")
+        }
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
     }
     
-    @IBAction func forgotPasswordButtonPressed() {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.welcomeLabelText = "Welcome, \(userNameTF.text ?? "")!"
     }
     
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        passwordTF.text = ""
+    }
+    
+    private func setAlertController(title: String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let cancelAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
 
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameTF {
+            passwordTF.becomeFirstResponder()
+        } else {
+            loginButtonPressed()
+        }
+        return true
+    }
 }
 
--- Login App/WelcomeViewController.swift
+++ Login App/WelcomeViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 class WelcomeViewController: UIViewController {
     @IBOutlet weak var welcomeLabel: UILabel!
     
+    var welcomeLabelText: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setGradientLayer()
+        welcomeLabel.text = welcomeLabelText
     }
-    @IBAction func logOutButtonPressed() {
+    
+    private func setGradientLayer() {
+        let startColor = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1).cgColor
+        let endColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1).cgColor
+        
+        let gradientLayer = CAGradientLayer()
+        
+        view.layer.insertSublayer(gradientLayer, at: 0)
+        gradientLayer.frame = view.frame
+        gradientLayer.colors = [startColor, endColor]
     }
 }
